47018ceddea2a543aca546a21350dde3393e2281
Leo <minarae@emart.com>
2025-03-11 14:30:58 +0900

    배포 테스트트

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Cloudflare Pages에 최적화된 설정
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
